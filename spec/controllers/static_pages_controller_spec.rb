f0386b6f8507892e0385be07926635b301acbf0e
Malachi Irwin <malachi@theirwins.ws>
2018-07-05 21:42:59 -0500

    add full_title helper

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   before do
-    @title = "| Ruby on Rails Tutorial Sample App"
+    @title = "Ruby on Rails Tutorial Sample App"
   end
 
   render_views
@@ -10,25 +10,25 @@ RSpec.describe StaticPagesController, type: :controller do
     it "gets home" do
       get :home
       expect(response).to have_http_status(:success)
-      assert_select "title", "Home #{@title}"
+      assert_select "title", "#{@title}"
     end
 
     it "gets help" do
       get :help
       expect(response).to have_http_status(:success)
-      assert_select "title", "Help #{@title}"
+      assert_select "title", "Help | #{@title}"
     end
 
     it "gets about" do
       get :about
       expect(response).to have_http_status(:success)
-      assert_select "title", "About #{@title}"
+      assert_select "title", "About | #{@title}"
     end
 
     it "gets contact" do
       get :contact
       expect(response).to have_http_status(:success)
-      assert_select "title", "Contact #{@title}"
+      assert_select "title", "Contact | #{@title}"
     end
 
 end
